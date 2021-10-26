05b275ca1916a7988ba2fe58c5d1fedf0978e48c
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2021-10-26 23:33:14 +0530

    Update and rename bipartite.py to UnionFind.py

---
+++ UnionFind.py
@@ -0,0 +1,37 @@
+class UnionFind:
+    def __init__(self, nodes):
+        self.nodes = nodes
+        self.roots = [i for i in range(nodes)]
+        self.size = [1 for _ in range(nodes)]
+
+    def union(self, p, q):
+        root1, root2 = self.find(p), self.find(q)
+        if root1 == root2:
+            return
+        
+        if self.roots[root1] < self.roots[root2]:
+            self.roots[root2] = self.roots[root1]
+            self.size[root1] += self.size[root2]
+            self.size[root2] = 0
+        else:
+            self.roots[root1] = self.roots[root2]
+            self.size[root2] += self.size[root1]
+            self.size[root1] = 0
+        
+        return True
+            
+    def find(self, r):
+        root = r
+        
+        while root != self.roots[root]:
+            root = self.roots[root]
+
+        while r != root:
+            new = r
+            self.roots[r] = root
+            r = new
+
+        return root
+
+    def is_connected(self, p, q):
+        return self.find(p) == self.find(q)
