91b6716bf08e4ce17b07d2cff6aa00b9f5808cef
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2021-11-14 16:27:49 +0530

    Update Bipartite.py

--- Bipartite.py
+++ Bipartite.py
@@ -11,7 +11,7 @@ class Graph:
         def dfs(at, color):
             q = [(at, color)]
             while q:
-                node, color = q.pop(0)
+                node, color = q.pop()
                 if node in seen:
                     if seen[node] != color:
                         return False
