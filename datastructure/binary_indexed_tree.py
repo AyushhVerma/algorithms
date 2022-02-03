6ec1b31de03fe3a3aacd6cab384d926fb0336209
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2022-02-03 15:03:47 +0530

    Create binary_indexed_tree.py

---
+++ datastructure/binary_indexed_tree.py
@@ -0,0 +1,10 @@
+# class BIT:
+#   def __init__(self, n):
+#     self.bit = [0] * (n + 1)
+  
+#   def update(self, val, index):
+#     while index < len(self.bit):
+#       self.bit[index] += val
+#       index += index & -index
+  
+#   def query(self, val)
