f27beeaee6450563f73993fc11b2ae9ed6b0a9c8
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2022-02-08 18:16:43 +0530

    Update binary_indexed_tree.py

--- datastructure/binary_indexed_tree.py
+++ datastructure/binary_indexed_tree.py
@@ -1,10 +1,19 @@
-# class BIT:
-#   def __init__(self, n):
-#     self.bit = [0] * (n + 1)
-  
-#   def update(self, val, index):
-#     while index < len(self.bit):
-#       self.bit[index] += val
-#       index += index & -index
-  
-#   def query(self, val)
+class BIT:
+    """construction can be done in O(n)"""
+    def __init__(self, n):
+        self.n = n
+        self.ft = [0 for i in range(0, n)]
+
+    def update(self, i, val):
+        """update data in index i in O(log n)"""
+        while i < self.n:
+            self.ft[i] += val
+            i += i & (-i)
+
+    def query(self, i): 
+        """query cumulative data from index 0 to i in O(log n)"""
+        res = 0
+        while i > 0:
+            res += self.ft[i]
+            i -= i & (-i)
+        return res
