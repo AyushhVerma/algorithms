810284c940d7393d3d06738cedb8901175fd8952
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2022-08-02 01:28:54 +0530

    Update TopSort.py

--- TopSort.py
+++ TopSort.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Graph:
     def __init__(self, nodes):
         self.graph = [[] for _ in range(nodes)]
@@ -6,8 +8,9 @@ class Graph:
     def add_edge(self, _from, to):
         self.graph[_from].append(to)
 
-    def top_sort(self):
-        stack = []
+    def topo_sort(self):
+        """dfs based topoSort"""
+        stack = deque()
         visited = [False] * self.nodes
 
         def dfs(at):
@@ -15,14 +18,17 @@ class Graph:
             for to in self.graph[at]:
                 if not visited[to]:
                     dfs(to)
-            stack.insert(0, at)
-
+            stack.appendleft(at)
 
         for i in range(self.nodes):
             if not visited[i]:
                 dfs(i)
         
         return stack
+    
+    def kahns_algo(self):
+        """bfs based topoSort"""
+        ...
 
 if __name__ == '__main__':
     graph = Graph(6)
@@ -32,4 +38,4 @@ if __name__ == '__main__':
     graph.add_edge(4, 1)
     graph.add_edge(2, 3)
     graph.add_edge(3, 1)
-    print(graph.top_sort())
+    print(graph.topo_sort())
