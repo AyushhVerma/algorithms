5781dc8a73ebea42d6b56bde5f451ac457f07350
Ayush Verma <54748755+AyushhVerma@users.noreply.github.com>
2022-08-03 13:06:23 +0530

    Update UnionFind.py

--- UnionFind.py
+++ UnionFind.py
@@ -1,28 +1,18 @@
 class UnionFind:
     def __init__(self, nodes):
         self.nodes = nodes
-        self.roots = [i for i in range(nodes)]
-        self.size = [1 for _ in range(nodes)]
+        self.roots = {}
 
     def union(self, p, q):
         root1, root2 = self.find(p), self.find(q)
         if root1 == root2:
             return
-        
-        if self.size[root1] < self.size[root2]:
-            self.roots[root2] = self.roots[root1]
-            self.size[root1] += self.size[root2]
-            self.size[root2] = 0
-        else:
-            self.roots[root1] = self.roots[root2]
-            self.size[root2] += self.size[root1]
-            self.size[root1] = 0
-        
+        self.roots[root2] = root1
         return True
     
     @property
     def count_root(self):
-        return sum(root[i] == i for i in range(self.nodes))
+        return sum(root == node for node, root in self.roots.items())
             
     def find(self, r):
         if r != self.roots[i]:
